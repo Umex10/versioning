c7b2ea524e6be06738ce1c05643119f663d5c3dc
Umejr Dzinovic <Umejr.Dzinovic@edu.fh-joanneum.at>
2026-04-22 10:31:51 +0200

    docs: add swagger for v1 and v2

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,28 @@ import express from 'express';
 import cors from 'cors';
 import v1Routes from './routes/v1';
 import v2Routes from './routes/v2';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const swaggerV1 = YAML.load(path.join(__dirname, './docs/swagger_v1.yaml'));
+const swaggerV2 = YAML.load(path.join(__dirname, './docs/swagger_v2.yaml'));
+
+app.use(
+  '/api/v1/docs', 
+  swaggerUi.serveFiles(swaggerV1), 
+  swaggerUi.setup(swaggerV1)
+);
+
+app.use(
+  '/api/v2/docs', 
+  swaggerUi.serveFiles(swaggerV2), 
+  swaggerUi.setup(swaggerV2)
+);
 
 // CRUD | Task (string)
 app.use("/api/v1", v1Routes);
